d2bcc26fdfd1d8b137dc9547408255f4fbd0ff5c
DimSelyutin <dragnilll78@gmail.com>
2022-10-25 16:34:53 +0300

    validator only

--- by/social/main/validator/controller/Validator.java
+++ by/social/main/validator/controller/Validator.java
@@ -3,16 +3,13 @@ package by.social.main.validator.controller;
 import java.util.ArrayList;
 import java.util.List;
 
-
-
 public class Validator {
-   
 
     public Validator() {
-       
+
     }
 
-    public void validate(RegInfo dataUser) throws ValidationException{
+    public void validate(RegInfo dataUser) throws ValidationException {
         List<String> uncorrectFieldsName = new ArrayList<>();
 
         Email email = new Email(dataUser.getEmail());
@@ -20,32 +17,22 @@ public class Validator {
         PhoneNumber phone = new PhoneNumber(dataUser.getPhone_number());
 
         if (!email.email_validation()) {
-            uncorrectFieldsName.add("email");       //неверное поле
-        } 
-        if(!pass.password_validation()){
-            uncorrectFieldsName.add("pass");        //неверное поле
-
-        } 
-        if(!phone.phone_validation()){
-            uncorrectFieldsName.add("phone");       //неверное поле
+            uncorrectFieldsName.add("email"); // неверное поле
         }
 
-        if(uncorrectFieldsName != null){
+        if (!pass.password_validation()) {
+            uncorrectFieldsName.add("pass"); // неверное поле
+
+        }
+
+        if (!phone.phone_validation()) {
+            uncorrectFieldsName.add("phone"); // неверное поле
+        }
+
+        if (uncorrectFieldsName != null) {
             throw new ValidationException(uncorrectFieldsName);
         }
 
-        
-       
     }
-   
-
-    
-
-   
-
-
-   
-
-
 
 }
