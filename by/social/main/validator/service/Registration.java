dc8951a8223e32fef93bcb7a2a33fc13abd6062c
DimSelyutin <dragnilll78@gmail.com>
2022-10-31 15:24:08 +0300

    del

--- by/social/main/validator/service/Registration.java
+++ by/social/main/validator/service/Registration.java
@@ -4,20 +4,27 @@ import java.util.List;
 
 import by.social.main.validator.Director;
 import by.social.main.validator.bean.UserInfo;
-import by.social.main.validator.service.serviceExeption.ValidationException;
+
 
 public class Registration {
+
     private UserInfo newUser;
+
     List<String> errors;
+
+
 
     public List<String> getErrors() {
         return errors;
     }
 
+
     public Registration(UserInfo newUser) {
         this.newUser = newUser;
     }
 
+
+    
     public void registerNewUser(Director director) {
         
         //////////
