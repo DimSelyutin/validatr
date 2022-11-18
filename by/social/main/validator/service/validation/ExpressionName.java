06e488325703312bc6392f9c052b7a2416ca8f7e
Selyutin Dmitriy <102747240+DimSelyutin@users.noreply.github.com>
2022-11-18 16:11:39 +0300

    Update ExpressionName.java

--- by/social/main/validator/service/validation/ExpressionName.java
+++ by/social/main/validator/service/validation/ExpressionName.java
@@ -1,5 +1,18 @@
 package by.social.main.validator.service.validation;
 
 public enum ExpressionName {
-    EMAIL_EXPRESSION, PASSWORD_EXPRESSION, PHONE_EXPRESSION;
+    EMAIL_EXPRESSION("^([\\w-\\.]+){1,64}@([\\w&&[^_]]+){2,255}.[a-z]{2,}$"),
+    PASSWORD_EXPRESSION("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\\W]).{6,20}"), 
+    PHONE_EXPRESSION("\\d{12}|(?:\\d{3}-){2}\\d{4}|\\(\\d{3}\\)\\d{3}-?\\d{4}");
+
+    private String regexStr;
+
+    
+    private ExpressionName(String regexStr){
+        this.regexStr = regexStr;
+    }
+
+    public String getRegexStr(){
+        return regexStr;
+    }
 }
