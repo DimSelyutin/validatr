ba52031ea8a086c3ad69cfcf18039dd6bcfbcf25
Selyutin Dmitriy <102747240+DimSelyutin@users.noreply.github.com>
2022-11-22 15:36:07 +0300

    Update Validation.java

--- by/social/main/validator/service/Validation.java
+++ by/social/main/validator/service/Validation.java
@@ -1,92 +1,78 @@
-package by.social.main.validator.service;
+package by.service;
 
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
-import java.time.format.ResolverStyle;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import by.social.main.validator.bean.UserInfo;
-import by.social.main.validator.service.validation.ExpressionName;
-import by.social.main.validator.service.validation.ValidationBuilder;
-
-
-
+import by.bean.NewUser;
+import by.builder.ValidationBuilder;
+import by.controler.RegularName;
+import by.service.serviceExeption.ValidationException;
 
 public class Validation implements ValidationBuilder {
-    
-    private UserInfo newUser;
-    private List<String> uncorrectFieldName;
-    private DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd-MM-uuuu", Locale.US)
-                                              .withResolverStyle(ResolverStyle.STRICT);
 
-    
+    private NewUser newUser;
+    private RegularName regex = new RegularName();
+    private List<String> uncorrectFieldName;
+
     public Validation() {
     }
 
-    public Validation(UserInfo newUser) {
+    public Validation(NewUser newUser) {
         this.newUser = newUser;
         this.uncorrectFieldName = new ArrayList<>();
     }
 
     @Override
-    public void validEmail() {
-        if (!validate(newUser.getEmail(), ExpressionName.EMAIL_EXPRESSION.getRegexStr())) {
+    public void validEmail(){
+        if (!valid(newUser.getEmail(), regex.getEMAIL_EXPRESSION())) {
             uncorrectFieldName.add("Uncorrect Email");
         }
 
     }
 
     @Override
-    public void validPhone() {
-        if (!validate(newUser.getPhoneNumber(), ExpressionName.PHONE_EXPRESSION.getRegexStr())) {
+    public void validPhone(){
+        if (!valid(newUser.getPhoneNumber(), regex.getPHONE_EXPRESSION())) {
             uncorrectFieldName.add("Uncorrect Phone");
+
         }
 
-        
     }
-    
+
     @Override
     public void validPassword() {
-        if (!validate(newUser.getPassword(), ExpressionName.PASSWORD_EXPRESSION.getRegexStr())) {
+        if (!valid(newUser.getPassword(), regex.getPASSWORD_EXPRESSION())) {
             uncorrectFieldName.add("Uncorrect Password");
-            
+
+
         }
 
+    }
+
+    public void getResault() throws ValidationException {
+        if (!uncorrectFieldName.isEmpty()) {
+            throw new ValidationException("Error input data!", uncorrectFieldName);
+        }
+        
+
 
     }
-    @Override
-    public void validDate() {
-        if(!validate(newUser.getDateBirthday())){
-            uncorrectFieldName.add("Uncorrect date");
-        }       
-    }
-    
-    public List<String> getResault() {
-        return uncorrectFieldName;
-        
-    }
-    
-    public boolean validate(String str, String s){
-        if(s != null) {
-            Pattern val_pattern = Pattern.compile(s);   //enum
+
+
+
+
+
+
+
+    private boolean valid(String str, String s) {
+        if (s != null) {
+            Pattern val_pattern = Pattern.compile(s); // enum
             Matcher match = val_pattern.matcher(str);
             return match.find();
         }
         return false;
     }
-
-    public boolean validate(String str){
-        try {
-            this.dateFormatter.parse(str);
-        } catch (DateTimeParseException e) {
-            return false;
-        }   
-        return true;
-    }
-
 
 }
